bf901722e4ca7433bb3ad101234b79de1576b80d
Augustin <augustin.bodet@insa-lyon.fr>
2021-01-30 10:39:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, url_for, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+
+def do_the_login():
+    pass
+
+
+def show_the_login_form():
+    pass
+
+#Pour verifier les URL avec le shell Python
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
